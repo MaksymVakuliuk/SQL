dfb81aea5aa9c6a1d6706c53b2075f5f0de44a6f
Maksym Vakuliuk <vakuliukmaksym@gmail.com>
2019-12-18 18:06:45 +0200

    Lab_4 change fix errors

--- Lab_4/SQLQuery_Lab_4.sql
+++ Lab_4/SQLQuery_Lab_4.sql
@@ -1,29 +1,30 @@
 ﻿USE Gifts_for_children_of_employees
 
 --1.	Вивести список всіх працівників, у яких є хоча б одна дитина.
+DROP PROCEDURE employeers_with_children
+
 GO
-CREATE PROCEDURE employees_with_childs AS
+CREATE PROCEDURE employeers_with_children AS
 BEGIN
 SELECT LastName FROM Employeers 
 LEFT JOIN ChildrenOfEmployeers
 ON Employeers.Employee_ID = ChildrenOfEmployeers.Employee_ID
 END
 
-EXEC employees_with_childs 
-
+EXEC employeers_with_children
 
 --2.	Вивести список дітей, яким відали подарунки в вказаний період.
-DROP PROCEDURE childs_with_gifts
+DROP PROCEDURE children_with_gifts
 
 GO
-CREATE PROCEDURE childs_with_gifts AS
+CREATE PROCEDURE children_with_gifts AS
 BEGIN
 SELECT FirstName FROM ChildrenOfEmployeers  AS COF LEFT JOIN Gifts AS GFT
 ON COF.Child_ID = GFT.Child_ID 
 WHERE GFT.Date_of_issue IS NOT NULL
 END
 
-EXEC childs_with_gifts 
+EXEC children_with_gifts 
 
 
 --3.	Вивести список батьків, я яких є неповнолітні діти.
